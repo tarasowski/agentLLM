fd010da071a94eda26edb346f20ec6a3ef0a1f16
tarasowski <dimitri+github@tarasowski.de>
2025-05-21 08:40:56 +0200

    feat: added tool definition

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
   "context"
   "fmt"
   "os"
+  "encoding/json"
   // add later other providers
   "github.com/anthropics/anthropic-sdk-go"
 )
@@ -20,14 +21,18 @@ func main() {
     return scanner.Text(), true
   }
 
-  agent := NewAgent(&client, getUserMessage)
+  tools := []ToolDefinition{}
+  agent := NewAgent(&client, getUserMessage, tools)
   err := agent.Run(context.TODO())
   if err != nil {
     fmt.Printf("Error: %s\n", err.Error())
   }
 }
 
-func NewAgent(client * anthropic.Client, getUserMessage func() (string, bool)) *Agent {
+func NewAgent(client * anthropic.Client, 
+              getUserMessage func() (string, bool),
+              tools []ToolDefinition,
+            ) *Agent {
   return &Agent{
     client:        client,
     getUserMessage: getUserMessage,
@@ -37,6 +42,7 @@ func NewAgent(client * anthropic.Client, getUserMessage func() (string, bool)) *
 type Agent struct {
   client        *anthropic.Client
   getUserMessage func() (string, bool)
+  tools        []ToolDefinition
 }
 
 func (a *Agent) Run(ctx context.Context) error {
@@ -73,15 +79,34 @@ func (a *Agent) Run(ctx context.Context) error {
 }
 
 func (a *Agent) runInference(ctx context.Context, conversation []anthropic.MessageParam) (*anthropic.Message, error) {
+  anthropicTools := []anthropic.ToolUnionParam{}
+
+  for _, tool := range a.tools {
+    anthropicTools = append(anthropicTools, anthropic.ToolUnionParam{
+      OfTool: &anthropic.ToolParam{
+        Name:         tool.Name,
+        Description:  anthropic.String(tool.Description),
+        InputSchema:  tool.InputSchema,
+      },
+    })
+  }
+
   message, err := a.client.Messages.New(ctx, anthropic.MessageNewParams{
     Model: anthropic.ModelClaude3_7SonnetLatest,
     MaxTokens: int64(1024),
     Messages: conversation,
+    Tools: anthropicTools,
   })
   return message, err
 }
 
 
+type ToolDefinition struct {
+  Name            string `json:"name"`
+  Description     string `json:"description"`
+  InputSchema     anthropic.ToolInputSchemaParam `json:"input_schema"`
+  Function        func(input json.RawMessage) (string, error)
+}
 
 
 
